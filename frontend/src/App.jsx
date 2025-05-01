309ba088557253a28943a7d84be01eadb544be41
hadiamoosa40 <hadiamoosa40@gmail.com>
2025-05-02 00:31:59 +0500

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,6 +10,10 @@ import Conversation from "./components/Content/Content";
 import Upgrade from "./components/Upgrade/upgrade";
 import Subscription from "./components/Subscription/Subscription";
 import Signup from "./components/SignIn/Signin";
+import Success from "./components/Sucess/Success";
+import Dashboard from "./components/Admin Dashbaord/Dashboard";
+import SettingsPage from "./components/Settings/Setting";
+import AnalyticsPage from "./components/Analytics/Analytics";
 
 const App = () => {
   return (
@@ -27,6 +31,11 @@ const App = () => {
             <Route path="/upgrade" element={<Upgrade />}/>
             <Route path="/subscription" element={<Subscription />}/>
             <Route path="/signup" element={<Signup/>}/>
+            <Route path="/success" element={<Success />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/settings" element={<SettingsPage />} />
+            <Route path="/analytics" element={<AnalyticsPage />} />
+
 
 
 
